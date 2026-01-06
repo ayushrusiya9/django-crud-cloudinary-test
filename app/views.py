6479837c4ac65894605788311d9a2c8c8fe8bb08
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-06 14:57:01 +0530

    implement-add-to-cart-functionality

--- app/views.py
+++ app/views.py
@@ -13,14 +13,30 @@ def home(request):
         students = Student.objects.all()
     return render(request, 'home.html',{'students': students})
 
+
 def student_list(request):
     query = request.GET.get('q')
-    if query:
-        students = Student.objects.filter(Q(name__icontains=query) | Q(city__icontains=query))
-    else:
-        students = Student.objects.all()
+    sort = request.GET.get('sort')
+    order = request.GET.get('order')
 
-    return render(request, 'student_list.html', {'students': students})
+    students = Student.objects.all()
+
+    if query:
+        students = students.filter(
+            Q(name__icontains=query) |
+            Q(city__icontains=query)
+        )
+
+    if sort in ['name', 'city']:
+        if order == 'desc':
+            students = students.order_by(f'-{sort}')
+        else:
+            students = students.order_by(sort)
+
+    return render(request, 'student_list.html', {
+        'students': students
+    })
+
 
 def student_create(request):
     if request.method == 'POST':
@@ -61,3 +77,49 @@ def student_delete(request, id):
     student.delete()
     messages.success(request, "Student deleted successfully")
     return redirect('student_list')
+
+
+def cart_add(request, id):
+    cart = request.session.get('cart', [])
+
+    cart.append(id)  # id ko cart mein add karo
+    request.session['cart'] = cart
+
+    return redirect('student_list')
+
+
+def cart_detail(request):
+    cart = request.session.get('cart', [])
+
+    # Step 1: quantity dictionary manually banao
+    counts = {}
+    for item in cart:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
+
+    # Step 2: students fetch karo
+    students = Student.objects.filter(id__in=counts.keys())
+
+    # Step 3: cart_items build karo
+    cart_items = []
+    for student in students:
+        cart_items.append({
+            'student': student,
+            'quantity': counts.get(student.id, 0)
+        })
+
+    return render(request, 'cart.html', {
+        'cart_items': cart_items
+    })
+
+
+def cart_remove(request, id):
+    cart = request.session.get('cart', [])
+
+    if id in cart:
+        cart.remove(id)   # sirf ek quantity kam
+
+    request.session['cart'] = cart
+    return redirect('cart_detail')
--- project/urls.py
+++ project/urls.py
@@ -25,4 +25,9 @@ urlpatterns = [
     path('add/', views.student_create, name='student_create'),
     path('edit/<int:id>/', views.student_update, name='student_update'),
     path('delete/<int:id>/', views.student_delete, name='student_delete'),
+
+    path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/remove/<int:id>/', views.cart_remove, name='cart_remove'),
+
 ]
